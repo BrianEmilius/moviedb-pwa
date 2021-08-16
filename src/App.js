83f0a47c9f8f362413b1270b54ec33cd2f1a78f7
Brian Emilius <brian@brianemilius.com>
2021-08-16 08:35:41 +0200

    test notifications

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Index from "./views/index";
 import SearchContext from "./store/searchContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Router } from "@reach/router";
 import "./App.css"
 import Movie from "./views/movie";
@@ -8,9 +8,24 @@ import Movie from "./views/movie";
 function App() {
 	var searchState = useState([]);
 
+  useEffect(function() {
+    Notification.requestPermission(function(status) {
+        console.log('Notification permission status:', status);
+    });
+  }, []);
+
+  function displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration().then(function(reg) {
+        reg.showNotification('Hello world!');
+      });
+    }
+  }
+
 	return (
 		<SearchContext.Provider value={searchState}>
 			<div className="App">
+        <button onClick={displayNotification}>Notify me</button>
 				<Router>
 					<Index path="/" />
 					<Movie path="/movie/:id" />
